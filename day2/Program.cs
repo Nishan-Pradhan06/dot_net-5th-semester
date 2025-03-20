b993717509dd7b95e7bd6a10ee98713bccb5b13f
nishanpradhan <nishanpradhan500@gmail.com>
2025-03-20 06:49:16 +0545

    Add project files.

---
+++ day2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String: ");
+            String  input = Console.ReadLine();
+            String reversed = ReverseString(input);
+        }
+    }
+}
