56d60ca7a02ec6ff26b60149911de796baea3863
nishanpradhan <nishanpradhan500@gmail.com>
2025-03-20 07:00:03 +0545

    revere string

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,12 +1,24 @@
-﻿namespace day2
+﻿namespace reverseString
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a String: ");
-            String  input = Console.ReadLine();
-            String reversed = ReverseString(input);
+           string myStr = Convert.ToString(Console.ReadLine())??string.Empty;
+            Console.WriteLine("Reversed String is: " + ReverseString(myStr));
+
+        }
+
+        public static string ReverseString( String str)
+        {
+            string rev = string.Empty;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                rev += str[i];
+
+            }
+            return rev;
         }
     }
 }
